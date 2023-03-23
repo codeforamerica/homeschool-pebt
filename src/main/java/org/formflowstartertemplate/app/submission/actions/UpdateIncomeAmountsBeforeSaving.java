12275debea6926c3992e6bb305bca1ed0cb6e84b
Cypress Borg <cborg@codeforamerica.org>
2023-03-23 17:29:37 -0500

    Update library version to new snapshot and fix conditions/actions

--- src/main/java/org/formflowstartertemplate/app/submission/actions/UpdateIncomeAmountsBeforeSaving.java
+++ src/main/java/org/formflowstartertemplate/app/submission/actions/UpdateIncomeAmountsBeforeSaving.java
@@ -5,11 +5,13 @@ import formflow.library.data.Submission;
 
 import java.util.ArrayList;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
 /**
  * This will update the data submitted for Income Amounts before its saved to the database. It will take the new Income Amount
  * data and reconcile it with the data stored in the database.
  */
+@Component
 public class UpdateIncomeAmountsBeforeSaving implements Action {
 
   public void run(Submission submission, String data) {
