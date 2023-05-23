6ca396f0909f6f52bf8d226deb829a975d48c945
Josh Stroud <jstroud@codeforamerica.org>
2023-05-23 07:57:31 -0700

    wip
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- src/main/java/org/homeschoolpebt/app/submission/conditions/IsJobSelfEmployed.java
+++ src/main/java/org/homeschoolpebt/app/submission/conditions/IsJobSelfEmployed.java
@@ -1,16 +1,13 @@
 package org.homeschoolpebt.app.submission.conditions;
 
-import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
-import java.util.ArrayList;
-import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
-public class IsJobHourly extends AbstractPebtCondition {
+public class IsJobSelfEmployed extends AbstractPebtCondition {
   public Boolean run(Submission submission, String uuid) {
     var item = currentSubflowItem(submission, uuid);
 
-    return item != null && item.getOrDefault("incomeIsJobHourly", "false").equals("true");
+    return item != null && item.getOrDefault("incomeWasSelfEmployed", "false").equals("true");
   }
 }
