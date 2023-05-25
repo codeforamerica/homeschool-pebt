09a343377a76f5263c7f31317250f5f6158c1879
Tom Dooner <tdooner@codeforamerica.org>
2023-05-25 16:15:36 -0700

    Fill unearned income PDF fields

--- src/main/java/org/homeschoolpebt/app/preparers/IncomePreparer.java
+++ src/main/java/org/homeschoolpebt/app/preparers/IncomePreparer.java
@@ -4,7 +4,7 @@ import formflow.library.data.Submission;
 import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
 import formflow.library.pdf.SubmissionFieldPreparer;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +13,20 @@ public class IncomePreparer implements SubmissionFieldPreparer {
 
   @Override
   public Map<String, SubmissionField> prepareSubmissionFields(Submission submission) {
-    List<Map<String, Object>> incomes = (List<Map<String, Object>>) submission.getInputData().get("income");
+    var fields = new HashMap<String, SubmissionField>();
 
-    if (incomes != null) {
-      var value = (String) incomes.get(0).get("incomeGrossMonthlyIndividual");
-      return Map.of("totalIncome", new SingleField("totalIncome", value, null));
-    } else {
-      return Map.of();
-    }
+    // unearned
+    fields.put("income-unemployment", new SingleField("income-unemployment", (String) submission.getInputData().get("incomeUnemploymentAmount"), null));
+    fields.put("income-workers-comp", new SingleField("income-workers-comp", (String) submission.getInputData().get("incomeWorkersCompensationAmount"), null));
+    fields.put("income-spousal-support", new SingleField("income-spousal-support", (String) submission.getInputData().get("incomeSpousalSupportAmount"), null));
+    fields.put("income-child-support", new SingleField("income-child-support", (String) submission.getInputData().get("incomeChildSupportAmount"), null));
+    fields.put("income-pension", new SingleField("income-pension", (String) submission.getInputData().get("incomePensionAmount"), null));
+    fields.put("income-retirement", new SingleField("income-retirement", (String) submission.getInputData().get("incomeRetirementAmount"), null));
+    fields.put("income-ssi", new SingleField("income-ssi", (String) submission.getInputData().get("incomeSSIAmount"), null));
+    fields.put("income-other", new SingleField("income-other", (String) submission.getInputData().get("incomeOtherAmount"), null));
+
+    // TODO: earned
+
+    return fields;
   }
 }
--- src/test/java/org/homeschoolpebt/app/preparers/IncomePreparerTest.java
+++ src/test/java/org/homeschoolpebt/app/preparers/IncomePreparerTest.java
@@ -4,21 +4,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import formflow.library.data.Submission;
 import formflow.library.pdf.SingleField;
-import java.util.ArrayList;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class IncomePreparerTest {
-
   @Test
-  void returnsCalculatedIncome() {
-    var subflowItems = new ArrayList<Map<String, Object>>();
-    subflowItems.add(Map.of("incomeGrossMonthlyIndividual", "123"));
+  void includesUnearnedIncome() {
+    Submission submission = Submission.builder().inputData(Map.of(
+      "incomeUnemploymentAmount", "111",
+      "incomeWorkersCompensationAmount", "222",
+      "incomeSpousalSupportAmount", "333",
+      "incomeChildSupportAmount", "444",
+      "incomePensionAmount", "555",
+      "incomeRetirementAmount", "666",
+      "incomeSSIAmount", "777",
+      "incomeOtherAmount", "888"
+    )).build();
 
-    Submission submission = Submission.builder().inputData(Map.of("income", subflowItems)).build();
     IncomePreparer preparer = new IncomePreparer();
-    assertThat(preparer.prepareSubmissionFields(submission)).isEqualTo(
-        Map.of("totalIncome", new SingleField("totalIncome", "123", null))
-    );
+    assertThat(preparer.prepareSubmissionFields(submission)).isEqualTo(Map.of(
+      "income-unemployment", new SingleField("income-unemployment", "111", null),
+      "income-workers-comp", new SingleField("income-workers-comp", "222", null),
+      "income-spousal-support", new SingleField("income-spousal-support", "333", null),
+      "income-child-support", new SingleField("income-child-support", "444", null),
+      "income-pension", new SingleField("income-pension", "555", null),
+      "income-retirement", new SingleField("income-retirement", "666", null),
+      "income-ssi", new SingleField("income-ssi", "777", null),
+      "income-other", new SingleField("income-other", "888", null)
+    ));
   }
 }
