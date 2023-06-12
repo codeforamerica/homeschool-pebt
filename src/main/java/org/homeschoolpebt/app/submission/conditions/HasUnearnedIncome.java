06cd2c00eb57ac7d37393ca527ad412f153a2ff8
Asheesh Laroia <alaroia@codeforamerica.org>
2023-06-12 17:24:25 -0400

    Only ask about unearned income if they have any
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

---
+++ src/main/java/org/homeschoolpebt/app/submission/conditions/HasUnearnedIncome.java
@@ -0,0 +1,40 @@
+package org.homeschoolpebt.app.submission.conditions;
+
+import formflow.library.data.Submission;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class HasUnearnedIncome extends AbstractPebtCondition {
+  public Boolean run(Submission submission, String uuid) {
+    if (submission == null) {
+      return false;
+    }
+
+    var inputData = submission.getInputData();
+    if (inputData == null) {
+      return false;
+    }
+
+    return hasContents(inputData.get("incomeUnearnedTypes")) || hasContents(inputData.get("incomeUnearnedRetirementTypes"));
+
+  }
+
+  private boolean hasContents(Object o) {
+    if (o == null) {
+      return false;
+    }
+
+    if (!(o instanceof List)) {
+      return false;
+    }
+
+    var l = (List) o;
+    if (l.size() == 0 || l.get(0) == null || l.get(0).equals("none")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
