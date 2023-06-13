b67fc3aac20c99e44ceb2c38f805818627aac34d
Martha Pidcock <mpidcock@codeforamerica.org>
2023-06-13 16:38:59 -0700

    Integrate Twilio and send an SMS confirmation message when application is signed

--- src/main/java/org/homeschoolpebt/app/submission/messages/ConfirmationMessage.java
+++ src/main/java/org/homeschoolpebt/app/submission/messages/ConfirmationMessage.java
@@ -23,11 +23,29 @@ public class ConfirmationMessage implements PebtMessage {
   @Override
   public Email renderEmail() {
     var applicationNumber = SubmissionUtilities.getFormattedConfirmationNumber(transmission.getConfirmationNumber());
-    String subject = "Test confirmation email";
+    var applicantFullName = SubmissionUtilities.applicantFullName(submission);
+    String subject = "Application Submitted for P-EBT 4.0";
+    String body = """
+      <html>
+        <body>
+          <p>Dear %s</p>
+          <p>Thank you for submitting your application for P-EBT benefits for the 2022-2023 school year. You should hear back from the California Department of Social Services by phone or mail in the next 2-4 weeks.</p>
+          <p>Your application ID is %s.</p>
+          <p>If you need food now, you can contact your local food bank at https://www.cafoodbanks.org/find-food.</p>
+          <p>- California Department of Social Services</p>
+        </body>
+      </html>
+      """.formatted(applicationNumber, applicantFullName);
+    return new Email(subject, body);
+  }
 
-    String emailBody = "<html><body><p>Your confirmation number is: <br>%s</p></body></html>".formatted(
-      applicationNumber
-    );
-    return new Email(subject, emailBody);
+  @Override
+  public Sms renderSms() {
+    var applicationNumber = SubmissionUtilities.getFormattedApplicationNumber(transmission.getApplicationNumber());
+    String body = """
+      Thank you for submitting the application for P-EBT benefits for the 2022-2023 school year. You will hear back within 2-4 weeks.\s
+      Your application ID is %s - CDSS (California Department of Social Services)
+      """.formatted(applicationNumber);
+    return new Sms(body);
   }
 }
