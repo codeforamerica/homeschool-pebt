fe139c037237b03048f1736fea77f2aa17b690e0
Tom Dooner <tdooner@codeforamerica.org>
2023-06-22 15:25:05 -0700

    Update footer to match mocks
    * Don't promise people a UBI
    * Update the logo and links
    * Delete the FAQ page

--- src/test/java/org/homeschoolpebt/app/journeys/StaticPagesJourneyTest.java
+++ src/test/java/org/homeschoolpebt/app/journeys/StaticPagesJourneyTest.java
@@ -1,7 +1,5 @@
 package org.homeschoolpebt.app.journeys;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.homeschoolpebt.app.utils.AbstractBasePageTest;
 import org.homeschoolpebt.app.utils.Page;
 import org.junit.jupiter.api.Tag;
@@ -16,25 +14,13 @@ public class StaticPagesJourneyTest extends AbstractBasePageTest {
 
   @Test
   void staticPagesJourney() {
+    var windowHandle = driver.getWindowHandle();
     // Landing screen
-    assertThat(testPage.getTitle()).isEqualTo("Get food money for students.");
-    assertThat(driver.getWindowHandles().size()).isEqualTo(1);
-    String originalWindow = driver.getWindowHandle();
-    // Go to FAQ tab
-    testPage.clickLink("FAQ");
-    assertThat(driver.getWindowHandles().size()).isEqualTo(2);
-    switchAwayFromOriginalWindow(originalWindow);
-    assertThat(testPage.getTitle()).isEqualTo("Frequently Asked Questions");
-    // Back on landing screen
-    driver.close();
-    driver.switchTo().window(originalWindow);
-    assertThat(testPage.getTitle()).isEqualTo("Get food money for students.");
-    assertThat(driver.getWindowHandles().size()).isEqualTo(1);
+    assertPageTitle("Get food money for students.");
     // Go to privacy policy tab
     testPage.clickLink("Privacy Policy");
-    assertThat(driver.getWindowHandles().size()).isEqualTo(2);
-    switchAwayFromOriginalWindow(originalWindow);
-    assertThat(testPage.getTitle()).isEqualTo("Privacy Policy");
+    switchAwayFromOriginalWindow(windowHandle);
+    assertPageTitle("Privacy Policy");
   }
 
   void switchAwayFromOriginalWindow(String originalWindow) {
