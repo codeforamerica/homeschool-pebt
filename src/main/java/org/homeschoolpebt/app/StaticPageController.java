fa1d4cbe2c9971d93f86885cc54a3182c2c68ab2
Tom Dooner <tdooner@codeforamerica.org>
2023-06-23 10:27:43 -0700

    Add redirect from /docs -> start of laterdocs flow
    ASANA-1204884286566889

--- src/main/java/org/homeschoolpebt/app/StaticPageController.java
+++ src/main/java/org/homeschoolpebt/app/StaticPageController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.info.GitProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 
@@ -37,6 +38,11 @@ public class StaticPageController {
     model.put("applicationsDisabled", applicationsDisabled.equals("true"));
 
     return new ModelAndView("index", model);
+  }
+
+  @GetMapping("/docs")
+  RedirectView getDocs() {
+    return new RedirectView("/flow/docUpload/addDocumentsSignpost");
   }
 
   /**
