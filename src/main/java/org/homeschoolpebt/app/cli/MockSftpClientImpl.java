167774b666b4a7f4ec70ef431fa6f0caaad37dc4
Sree <72890349+sree-cfa@users.noreply.github.com>
2023-06-26 12:10:43 -0700

    Schedule sftp transmit submissions (#198)
    Co-authored-by: Tom Dooner <tdooner@codeforamerica.org>

--- src/main/java/org/homeschoolpebt/app/cli/MockSftpClientImpl.java
+++ src/main/java/org/homeschoolpebt/app/cli/MockSftpClientImpl.java
@@ -6,16 +6,19 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-@Profile("test")
+@Profile("!production")
 public class MockSftpClientImpl implements SftpClient {
 
   @Override
   public void uploadFile(String zipFilename) {
     // Do nothing
+    log.info("Mock uploading file " + zipFilename);
   }
 }
