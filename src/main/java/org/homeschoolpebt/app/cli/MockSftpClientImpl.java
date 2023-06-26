eb183d687624602b5ca53a7ff197ad38459b630f
Sree P <sprasad@codeforamerica.org>
2023-06-26 12:05:48 -0700

    adding log

--- src/main/java/org/homeschoolpebt/app/cli/MockSftpClientImpl.java
+++ src/main/java/org/homeschoolpebt/app/cli/MockSftpClientImpl.java
@@ -6,10 +6,12 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Profile("test")
 public class MockSftpClientImpl implements SftpClient {
@@ -17,5 +19,6 @@ public class MockSftpClientImpl implements SftpClient {
   @Override
   public void uploadFile(String zipFilename) {
     // Do nothing
+    log.info("Mock uploading file " + zipFilename);
   }
 }
