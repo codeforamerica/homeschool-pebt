0689d5b9cf9b0a261b2898c463ebc6e634ae0542
Cypress Borg <cborg@codeforamerica.org>
2023-07-31 13:14:40 -0500

    Add chrome driver path to selenium
    Co-authored-by: lkemperman-cfa <lkemperman@codeforamerica.org>

--- src/test/java/org/homeschoolpebt/app/utils/SeleniumFactory.java
+++ src/test/java/org/homeschoolpebt/app/utils/SeleniumFactory.java
@@ -39,6 +39,7 @@ public class SeleniumFactory implements FactoryBean<RemoteWebDriver> {
     HashMap<String, Object> chromePrefs = new HashMap<>();
     chromePrefs.put("download.default_directory", tempdir.toString());
     options.setExperimentalOption("prefs", chromePrefs);
+    options.setBinary(WebDriverManager.chromedriver().getBrowserPath().get().toString());
     options.addArguments("--window-size=1280,2000");
     options.addArguments("--headless=new");
     options.addArguments("--remote-allow-origins=*");
