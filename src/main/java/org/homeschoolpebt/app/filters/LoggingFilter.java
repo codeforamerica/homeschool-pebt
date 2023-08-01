26700f8c3d3bdbeb520e3067b1c9162119a6c7ff
Tom Dooner <tdooner@codeforamerica.org>
2023-08-01 15:49:47 -0700

    Add submissionId to JSON logs
    This configures the LoggingFilter to get the "id" attribute from the
    session and add it to the log metadata blob.

--- src/main/java/org/homeschoolpebt/app/filters/LoggingFilter.java
+++ src/main/java/org/homeschoolpebt/app/filters/LoggingFilter.java
@@ -1,17 +1,15 @@
 package org.homeschoolpebt.app.filters;
 
 
-import java.io.IOException;
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.joda.time.DateTime;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class LoggingFilter implements Filter {
@@ -25,8 +23,12 @@ public class LoggingFilter implements Filter {
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                        FilterChain filterChain) throws ServletException, IOException {
     HttpServletRequest request = (HttpServletRequest) servletRequest;
-    MDC.put("sessionId", request.getSession().getId());
-    MDC.put("sessionCreatedAt", new DateTime(request.getSession().getCreationTime()).toString("HH:mm:ss.SSS"));
+    HttpSession session = request.getSession();
+    UUID id = (UUID) session.getAttribute("id");
+
+    MDC.put("sessionId", session.getId());
+    MDC.put("submissionId", id == null ? "null" : id.toString());
+    MDC.put("sessionCreatedAt", new DateTime(session.getCreationTime()).toString("HH:mm:ss.SSS"));
     MDC.put("method", request.getMethod());
     MDC.put("request", request.getRequestURI());
     filterChain.doFilter(servletRequest, servletResponse);
