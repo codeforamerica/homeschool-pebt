50f949d6d1dd85986c8e8fdd1b209553f85e39df
Tom Dooner <tdooner@codeforamerica.org>
2023-08-10 15:14:07 -0700

    Add message body & subject

--- src/main/java/org/homeschoolpebt/app/submission/messages/LastMinuteReminderMessage.java
+++ src/main/java/org/homeschoolpebt/app/submission/messages/LastMinuteReminderMessage.java
@@ -21,20 +21,26 @@ public class LastMinuteReminderMessage implements PebtMessage {
     String subject;
     String body;
     if (getSubmissionLanguage(submission).equals("es")) {
-      subject = "TODO";
+      subject = "Tu solicitud para P-EBT 4.0 está incompleta (¡Las solicitudes cierran el 15 de agosto!)";
       body = """
         <html>
           <body>
-            TODO
+            <p>Estimado %s,</p>
+            <p>Gracias por tu interés en el programa P-EBT 4.0 de California. ¡Parece que comenzaste la solicitud, pero falta que la termines. Este es un recordatorio de que la solicitud se cerrará el martes 15 de agosto. Si aún deseas aplicar, visita la página <a href="https://www.getpebt.org">https://www.getpebt.org</a>.
+            <p>Gracias por su interés en nuestro programa.</p>
+            <p>- El Departamento de Servicios Sociales de California</p>
           </body>
         </html>
         """.formatted(StringUtils.escapeXml(applicantFullName));
     } else {
-      subject = "TODO";
+      subject = "Your PEBT 4.0 application is incomplete (Applications close on August 15th!)";
       body = """
         <html>
           <body>
-            TODO
+            <p>Dear %s,</p>
+            <p>Thank you for your interest in California's P-EBT 4.0 program. It looks like you started an application, but didn't finish it yet. This is a friendly reminder that the application will close on <strong>Tuesday, August 15th</strong>. If you still want to apply, go to <a href="https://www.getpebt.org">https://www.getpebt.org</a>.</p>
+            <p>Thank you for your interest in our program.</p>
+            <p>- California Department of Social Services</p>
           </body>
         </html>
         """.formatted(StringUtils.escapeXml(applicantFullName));
